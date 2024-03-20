6234ec6c97a483d1f246d0127cb83a2f6076d303
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2024-03-20 15:08:50 +0100

    utils

--- Lean4Lean/Commands.lean
+++ Lean4Lean/Commands.lean
@@ -1,40 +1,10 @@
 import Lean
 import Lean4Lean.Replay
+import Lean4Lean.Util
 
-open Lean Elab Command Term Meta
+open Lean
 
--- TODO existing function for this?
-partial def Lean.Name.isCStage : Name → Bool
-| .str p s   => s.startsWith "_cstage" || p.isCStage
-| .num p _   => p.isCStage
-| .anonymous => false
-
-namespace Deps
-  structure Context where
-    env        : Environment
-
-  structure State where
-    map : HashMap Name ConstantInfo := {}
-  abbrev DepsM := ReaderT Context <| StateRefT State IO
-
-  @[inline] def DepsM.run (x : DepsM α) (ctx : Context) (s : State := {}) : MetaM (α × State) :=
-    x ctx |>.run s
-
-  @[inline] def DepsM.toIO (x : DepsM α) (ctxCore : Lean.Core.Context) (sCore : Lean.Core.State) (ctx : Context) (s : State := {}) : IO (α × State) := do
-  let ((a, s), _, _) ← (x.run ctx s).toIO ctxCore sCore
-  pure (a, s)
-
-  partial def namedConstDeps (name : Name) : DepsM Unit := do
-    match ((← get).map.find? name) with
-    | .none =>
-      let some const := (← read).env.find? name | throw $ IO.userError s!"could not find constant \"{name}\" for translation, verify that it exists in the Lean input"
-      modify fun s => { s with map := s.map.insert name const }
-      let deps := const.getUsedConstantsAsSet
-      for dep in deps do
-        if !dep.isImplementationDetail && !dep.isCStage then
-          namedConstDeps dep
-    | .some _ => pure default
-end Deps
+namespace Lean4Lean
 
 def checkConstants (env : Lean.Environment) (consts : Lean.NameSet) (printErr := false) : IO Lean.NameSet := do
   let mut onlyConstsToTrans : Lean.NameSet := default
@@ -68,9 +38,11 @@ def checkConstants (env : Lean.Environment) (consts : Lean.NameSet) (printErr :=
       errConsts := errConsts.insert const
   pure onlyConstsToTrans
 
+end Lean4Lean
+
 elab "#check_l4l " i:ident : command => do
   let env ← getEnv
-  discard $ checkConstants (printErr := true) env (.insert default i.getId)
+  discard $ Lean4Lean.checkConstants (printErr := true) env (.insert default i.getId)
   -- match macroRes with
   -- | some (name, _) => logInfo s!"Next step is a macro: {name.toString}"
   -- | none =>
